7a6d570079e954634026857c778c429eecf1d01f
mugi <samuelmugi@gmail.com>
2024-04-06 13:21:15 +0300

    init commit

---
+++ src/main/java/com/mugi/logicea/cards/MugicardsApplication.java
@@ -0,0 +1,13 @@
+package com.mugi.logicea.cards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MugicardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MugicardsApplication.class, args);
+	}
+
+}
